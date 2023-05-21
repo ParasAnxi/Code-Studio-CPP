ef660aa131076f49ff2426833c6edf7dd5b1b164
ParasAnxi <paras.bhandari1010@gmail.com>
2023-05-21 10:29:34 +0530

    s

---
+++ Sort a stack.cpp
@@ -0,0 +1,20 @@
+#include <bits/stdc++.h> 
+void sortStack(stack<int>& s , int e){
+    if(s.empty() || (!s.empty() && s.top()<e)){
+        s.push(e);
+        return;
+    }
+    int n = s.top();
+    s.pop();
+    sortStack(s,e);
+    s.push(n);
+}
+void sortStack(stack<int> &stack){
+    if(stack.empty()){
+        return;
+    }
+    int num = stack.top();
+    stack.pop();
+    sortStack(stack);
+    sortStack(stack,num);
+}
