2819156862668917279fbc0d1e2fae01f11d5df8
ParasAnxi <paras.bhandari1010@gmail.com>
2023-05-21 11:24:07 +0530

    s

---
+++ Power Set.cpp
@@ -0,0 +1,28 @@
+#include <bits/stdc++.h>
+vector<vector<int>> helper(vector<int> &v, int i)
+{
+    if (i >= v.size())
+        return {{}};
+    vector<vector<int>> partialans = helper(v, i + 1);
+    vector<vector<int>> ans;
+    for (vector<int> x : partialans)
+    {
+        vector<int> as;
+        ans.push_back(x);
+    }
+    for (vector<int> x : partialans)
+    {
+        vector<int> temp;
+        temp.push_back(v[i]);
+        for (int y : x)
+        {
+           temp.push_back(y);
+        }
+       ans.push_back(temp);
+    }
+    return ans;
+}
+vector<vector<int>> pwset(vector<int> v)
+{
+    return helper(v, 0);
+}
