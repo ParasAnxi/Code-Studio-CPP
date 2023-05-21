4548a030d8cd5d3788a5f4970aa5c3ca9aca59cc
ParasAnxi <paras.bhandari1010@gmail.com>
2023-05-21 11:08:43 +0530

    d

---
+++ Single Element in a Sorted Array.cpp
@@ -0,0 +1,24 @@
+#include <bits/stdc++.h> 
+int uniqueElement(vector<int> arr, int n)
+{
+    int start = 0;
+    int end = n-1;
+    int mid = start + (end - start )/2;
+    while (start < end){
+          if (mid % 2 == 0) {
+            if(arr[mid] == arr[mid +1]){
+            start = mid + 2 ;
+            } 
+            else
+            end = mid ;
+          }
+          else {
+            if (arr[mid] == arr[mid - 1]) {
+              start = mid + 1;
+            } else
+              end = mid - 1;
+          }
+          mid = start + (end - start )/2;
+        }
+                  return arr[start];
+}
