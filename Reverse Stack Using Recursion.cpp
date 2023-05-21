dbc13a42e7538c085fcaff0548bdf3e724504442
ParasAnxi <paras.bhandari1010@gmail.com>
2023-05-21 10:31:13 +0530

    rs

---
+++ Reverse Stack Using Recursion.cpp
@@ -0,0 +1,19 @@
+void insertAtBottom(stack<int>& s ,int e){
+    if(s.empty()){
+        s.push(e);
+        return;
+    }
+    int num = s.top();
+    s.pop();
+    insertAtBottom(s,e);
+    s.push(num);
+}
+void reverseStack(stack<int> &stack) {
+    if(stack.empty()){
+        return;
+    }
+    int num = stack.top();
+    stack.pop();
+    reverseStack(stack);
+    insertAtBottom(stack,num);
+}
